1d5068ee6091a05a3920b51f8b99e47a8fec1a06
dmchoi77 <dm.choi.dev@gmail.com>
2024-08-13 00:25:32 +0900

    chore: 기타

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Request, Post, UseGuards, Body } from '@nestjs/common';
+import { Controller, Post, Body, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { User } from 'src/user/user.entity';
 
@@ -7,12 +7,15 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  async login(@Body() body) {
-    const user = await this.authService.validateUser(body.email, body.password);
-    if (user) {
-      return this.authService.login(user);
+  async login(@Body() loginDto: User) {
+    const user = await this.authService.validateUser(
+      loginDto.username,
+      loginDto.password,
+    );
+    if (!user) {
+      throw new UnauthorizedException();
     }
-    return { message: 'Invalid credentials' };
+    return this.authService.login(user);
   }
 
   @Post('register')
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,20 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
-import { JwtService } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: 'secretKey',
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
   providers: [AuthService, JwtService],
+  exports: [AuthService],
   controllers: [AuthController],
 })
 export class AuthModule {}
