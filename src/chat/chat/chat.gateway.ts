a02c140f5da2660c44ae69d27f644506bacd6592
dmchoi77 <dm.choi.dev@gmail.com>
2024-08-13 23:21:47 +0900

    fix: 프로젝트 이름 변경, chat gateway 불필요 코드 수정

--- src/chat/chat/chat.gateway.ts
+++ src/chat/chat/chat.gateway.ts
@@ -3,12 +3,9 @@ import {
   WebSocketServer,
   OnGatewayConnection,
   SubscribeMessage,
-  OnGatewayInit,
-  OnGatewayDisconnect,
 } from '@nestjs/websockets';
 import { JwtService } from '@nestjs/jwt';
 import { Server, Socket } from 'socket.io';
-import { Injectable } from '@nestjs/common';
 
 @WebSocketGateway({
   namespace: 'chat',
@@ -16,33 +13,28 @@ import { Injectable } from '@nestjs/common';
     origin: '*',
   },
 })
-@Injectable()
-export class ChatGateway implements OnGatewayInit, OnGatewayConnection {
+export class ChatGateway implements OnGatewayConnection {
   @WebSocketServer()
   server: Server;
 
-  afterInit(server: Server) {
-    console.log('🚀 ~ ChatGateway ~ afterInit ~ server:', server);
-  }
-
   constructor(private jwtService: JwtService) {}
 
-  async handleConnection(client: Socket, ...args: any[]) {
-    console.log('🚀 ~ ChatGateway ~ handleConnection ~ args:', args);
+  async handleConnection(client: Socket) {
     try {
       const token = client.handshake.query.token;
-      const payload = this.jwtService.verify(token as string);
+      const payload = this.jwtService.verify(token as string, {
+        secret: 'secretKey',
+      });
       client.data.user = payload;
     } catch (e) {
       client.disconnect();
     }
   }
-
   @SubscribeMessage('message')
-  handleMessage(client: Socket, payload: string): void {
+  handleMessage(client: Socket, payload: any): void {
     this.server.emit('message', {
       user: client.data.user.username,
-      message: payload,
+      ...payload,
     });
   }
 }
